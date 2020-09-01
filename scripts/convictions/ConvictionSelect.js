67f6ed254a85ce58f72db480cc87387eb94c5c75
bcdavis <bcollinsdavis@gmail.com>
2020-09-01 14:48:02 -0500

    implemented working crime filter selector and adjusted CSS some more

--- scripts/convictions/ConvictionSelect.js
+++ scripts/convictions/ConvictionSelect.js
@@ -3,17 +3,64 @@
 
 import { useConvictions, getConvictions} from "./ConvictionProvider.js"
 
+/*
+    Which element in your HTML contains all components?
+    That's your Event Hub. Get a reference to it here.
+*/
+const eventHub = document.querySelector(".container")
+const contentTarget = document.querySelector(".filters__crime")
+
+// On the event hub, listen for a "change" event.
+eventHub.addEventListener("change", event => {
+
+    // Only do this if the `crimeSelect` element was changed
+    if (event.target.id === "crimeSelect") {
+        // Create custom event. Provide an appropriate name.
+        const customEvent = new CustomEvent("crimeChosen", {
+            detail: {
+                crimeThatWasChosen: event.target.value,
+                crimeId: event.target.id
+            }
+        })
+
+        // Dispatch to event hub
+        eventHub.dispatchEvent(customEvent)
+    }
+})
+
+
+// const render = convictionsCollection => {
+//     contentTarget.innerHTML = `
+//         <select class="dropdown" id="crimeSelect">
+//             <option value="0">Please select a crime...</option>
+//             ... you wrote awesome code here ...
+//         </select>
+//     `
+// }
+
+
+// const ConvictionSelect = () => {
+//     getConvictions()
+//         .then(() => {
+//             const convictions = useConvictions()
+//             render(convictions)
+//         })
+// }
+
+
 const createSelectorOptions = (crimesArray) => {
-    const innerElement = document.querySelector(".filters__crime > .selector");
-    let HTMLCrimeArray = crimesArray.map(crime => {
-        return `
+    contentTarget.innerHTML = `
+    <select id="crimeSelect">
+        <option value="none">Please select a crime...</option>
+        ${
+            crimesArray.map(crime => `
             <option value="${crime.name}">${crime.name}</option>
-                
-        ` 
-        // create HTML for an individual officer for each officer in officersArray, and return it in a new array (HTMLArray)
-    })
-    innerElement.innerHTML = `<option value="none">Please select a crime...</option>`
-    innerElement.innerHTML += HTMLCrimeArray.join(" "); // this line REPLACES whatever is currently in the inner HTML of contentElement
+            `).join("")
+        }
+        
+    </selector>`
+    //innerElement.innerHTML = `<option value="none">Please select a crime...</option>`
+    //innerElement.innerHTML += HTMLCrimeArray.join(" "); // this line REPLACES whatever is currently in the inner HTML of contentElement
 }
 
 
@@ -21,9 +68,7 @@ export const CreateCrimeSelector = () => {
     getConvictions()
     .then(() => {
         const convictionsArray = useConvictions();
-        // create the selector element
-        document.querySelector(".filters__crime").innerHTML = `<select class="selector"></selector>`;
-        // add the dropdown options
+        // create the selector element and add the dropdown options
         createSelectorOptions(convictionsArray);
     })
 }
--- scripts/criminals/CriminalList.js
+++ scripts/criminals/CriminalList.js
@@ -3,6 +3,32 @@ import { getCriminals, useCriminals } from "./CriminalProvider.js"
 import {CriminalHTML} from "./Criminal.js"
 
 
+
+const eventHub = document.querySelector(".container");
+
+
+// Listen for the custom event you dispatched in ConvictionSelect
+eventHub.addEventListener("crimeChosen", event => {
+    // You remembered to add the id of the crime to the event detail, right?
+    if ("crimeId" in event.detail) {
+        console.log("event.detail.crimeThatWasChosen", event.detail.crimeThatWasChosen);
+        /*
+            Filter the criminals application state down to the people that committed the crime
+        */
+        const criminalsToFilter = useCriminals()
+        const matchingCriminals = criminalsToFilter.filter(currentCriminal => {
+            return currentCriminal.conviction === event.detail.crimeThatWasChosen
+        })
+
+        /*
+            Then invoke render() and pass the filtered collection as
+            an argument
+        */
+        addCriminalsToDOM(matchingCriminals);
+
+    }
+})
+
 const addCriminalsToDOM = (criminalsArray) => {
     const contentElement = document.querySelector(".criminalsContainer");
     let HTMLArray = criminalsArray.map(criminal => {
